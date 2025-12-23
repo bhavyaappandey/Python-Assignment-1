ab01af6c4c6dd9efbbca70a4d4bf675602b873b1
bhavyaappandey <bhavyaappandey@gmail.com>
2025-12-23 18:39:10 +0530

    task2.py

--- task2.py
+++ task2.py
@@ -3,4 +3,4 @@
 first_name = input("Enter first name:")
 second_name = input("Enter last name:")
 
-print("Hello", first_name, second_name)
+print(f"Hello, {first_name} {last_name}! Welcome to the Python program.")
